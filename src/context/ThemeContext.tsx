a1dabdcf00adb978d2db568ce12bb252d3dcfbe1
abuayaan01 <abuayaan01@gmail.com>
2026-06-21 02:23:45 +0530

    fix(theme): persist theme

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -9,9 +9,16 @@ type ThemeType = "light" | "dark";
 const ThemeContext = createContext<ThemeContextType | null>(null);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState<ThemeType>("light");
+  const [theme, setTheme] = useState<ThemeType>(() => {
+    const theme = localStorage.getItem("theme");
+    return theme ? theme as ThemeType : "light";
+  });
   function toggleTheme() {
-    setTheme((prev) => (prev == "light" ? "dark" : "light"));
+    setTheme((prev) => {
+      const theme = prev == "light" ? "dark" : "light";
+      localStorage.setItem("theme", theme);
+      return theme;
+    });
   }
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
